1413d1156bf4cf11e95d98a9ec1371b7c118774c
Maqi-x <mateusz.wujtowicz.mmm.2@gmail.com>
2024-12-01 14:12:02 +0100

    Build Script update

--- scripts/make_compiler.py
+++ scripts/make_compiler.py
@@ -1,4 +1,17 @@
+# -------------------------------------------------------------------- #
+#                         Builder for Project UEF                      #
+# -------------------------------------------------------------------- #
 import subprocess as sp
-sp.run([
-    "gcc", "-c", "compiler/main.cpp", "-o", "compiler/main.exe"
-])
+import platform
+
+os = platform.system()
+
+if os == "Linux" or os == "Darwin":
+    sp.run([
+        "gcc", "-c", "compiler/main.cpp", "-Icompiler/include", "-o", "compiler/main.bin"
+    ])
+if os == "Windows":
+    sp.run([
+        "gcc", "-c", "compiler/main.cpp", "-Icompiler\\include", "-o", "compiler/main.exe"
+    ])
+pass
