3845ca7500e8a90448ea5be702dfb97226243748
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2024-12-01 15:20:19 +0100

    Update main.cpp

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -1,9 +1,29 @@
-#include <version.h>
-#include <multiplatform.h>
+#include <version.hpp>
+#include <multiplatform.hpp>
 
 int main(int argc, char* argv[]) {
+    set_console_color(7, 0);
+
     if (argc == 0) {
+        print("uefc: ");
         set_console_color(4, 0);
-        return;
+        print_line("no arguments");
+        set_console_color(7, 0);
+        print_line("tip: you can use `uefc --version` to get compiler version");
+        return 0;
     }
+
+    for (int i = 1; i < argc; ++i) {
+        if (argv[i] == "--version") {
+            print("uefc (UniversalExecutableFormat compiler) ");
+            println(COMPILER_VERSION);
+            println("Licensed under GPL in version 3.");
+        }
+        if (argv[i] == "--authors") {
+            print("Created by UniversalExecutableFormat team (under the leadership of Gorciu).");
+        }
+    }
+
+    set_console_color(7, 0);
+    return 0;
 }
